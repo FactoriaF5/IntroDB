5389f2e994ed49e673376271a120b1025cf3b1ab
Sergi Virgili <svirgilif@gmail.com>
2020-10-28 14:22:03 +0100

    fix

--- src/Models/Student.php
+++ src/Models/Student.php
@@ -3,20 +3,25 @@
 
 namespace App\Models;
 
-use DateTime;
+
 use App\Database;
 
 class Student
 {
-    private int $id;
+    private ?int $id;
     private string $name;
-    private DateTime $created_at;
+    private ?string $created_at;
     private $database;
+    private $table = "students";
 
-    public function __construct(string $name)
+    public function __construct(string $name = 'nombre', int $id = null, string $created_at = null)
     {
         $this->name = $name;
-        $this->database = new Database();
+        $this->id = $id;
+        $this->created_at = $created_at;
+        if (!$this->database) {
+            $this->database = new Database();
+        }
     }
 
     public function getName()
@@ -24,12 +29,31 @@ class Student
         return $this->name;
     }
 
-    public function save()
+    public function getId()
     {
-        $this->database->mysql->query("INSERT INTO `students` (`name`) VALUES ('$this->name');");
+        return $this->id;
     }
 
-    public static function all()
+    public function getCreatedAt()
     {
+        return $this->created_at;
+    }
+
+    public function save(): void
+    {
+        $this->database->mysql->query("INSERT INTO `{$this->table}` (`name`) VALUES ('$this->name');");
+    }
+
+    public function all()
+    {
+        $query = $this->database->mysql->query("select * FROM {$this->table}");
+        $studentsArray = $query->fetchAll();
+        $studentList = [];
+        foreach ($studentsArray as $student) {
+            $studentItem = new Student($student["name"], $student["id"], $student["created_at"]);
+            array_push($studentList, $studentItem);
+        }
+
+        return $studentList;
     }
 }
