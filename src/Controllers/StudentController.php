9e908f18987e4eba0bb7d56f72b97bfd0bc4169a
Sergi Virgili <svirgilif@gmail.com>
2020-10-29 18:18:06 +0100

    Create method rendering

--- src/Controllers/StudentController.php
+++ src/Controllers/StudentController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Core\Views;
 use App\Models\Student;
-
+use phpDocumentor\Reflection\DocBlock\Tag;
 
 class StudentController
 {
@@ -30,11 +30,18 @@ class StudentController
         <li> {$student->getId()} - {$student->getName()} - {$student->getCreatedAt()} - delete </li>";
         }
         echo
-            "</ul> <a href='src/Controllers/CreateStudent.php'>NUEVO</a>";
+            "</ul> <a href='?action=create'>NUEVO</a>";
     }
 
     public function create()
     {
-        echo "create";
+        echo  <<<TAG
+        <h1>Nuevo Estudiante</h1>
+
+        <form action=CreateStudent.php" method="post">
+            <input type="text" name="name">
+            <input type="submit" value="Crear">
+        </form>
+        TAG;
     }
 }
