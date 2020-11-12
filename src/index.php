8d68a8f289206807c5fc0602f336ecf2d5b28c8a
Sergi Virgili <svirgilif@gmail.com>
2020-11-12 17:34:47 +0100

    simple ApiRest int

--- src/index.php
+++ src/index.php
@@ -3,6 +3,14 @@
 namespace App;
 
 use App\Controllers\StudentController;
+use App\Controllers\ApiStudentController;
 
+// if (assert($_GET)) {
+//     echo "por get";
+// }
+// if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+//     echo "delete method";
+// }
 
-$controller = new StudentController();
+//$controller = new StudentController();
+$controller = new ApiStudentController();
