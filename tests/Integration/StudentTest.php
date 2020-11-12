57078813abe072a3a64903384b1929f26e1fa31d
Sergi Virgili <svirgilif@gmail.com>
2020-11-12 18:00:06 +0100

    test can retrieve an Studentlist

--- tests/Integration/StudentTest.php
+++ tests/Integration/StudentTest.php
@@ -4,15 +4,36 @@ namespace Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 use App\Models\Student;
+use App\Database;
 
 
 class StudentTest extends TestCase
 {
+    private $db;
+
+    private function initDB()
+    {
+        $db = new Database();
+        $db->mysql->query("DELETE FROM `students`");
+        $db->mysql->query("ALTER TABLE `students` AUTO_INCREMENT = 1");
+        $this->db = $db;
+    }
+
+    public function setUp(): void
+    {
+        $this->initDB();
+    }
 
     public function test_can_retrieve_Student_list()
     {
+        $this->db->mysql->query("INSERT INTO `students` (`name`) VALUES ('Joana')");
+        $this->db->mysql->query("INSERT INTO `students` (`name`) VALUES ('Fritz')");
         $studentList = Student::all();
 
-        $this->assertEquals("Joana", $student->getName());
+        $this->assertEquals("Joana", $studentList[0]->getName());
+        $this->assertEquals("Fritz", $studentList[1]->getName());
+        $this->assertEquals(1, $studentList[0]->getId());
+        $this->assertEquals(2, $studentList[1]->getId());
+        $this->assertIsString($studentList[1]->getCreatedAt());
     }
 }
