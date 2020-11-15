a0db3cfea9b1e79fc631c19ef281329eec8afccf
Sergi Virgili <svirgilif@gmail.com>
2020-11-15 12:47:35 +0100

    Intro to Integration Tests

--- tests/Integration/StudentTest.php
+++ tests/Integration/StudentTest.php
@@ -3,12 +3,12 @@
 namespace Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
-use App\Models\Student;
 use App\Database;
-
+use App\Models\Student;
 
 class StudentTest extends TestCase
 {
+    // baciar base de datos
     private $db;
 
     private function initDB()
@@ -24,15 +24,15 @@ class StudentTest extends TestCase
         $this->initDB();
     }
 
-    public function test_can_retrieve_Student_list()
+    public function test_can_retrive_an_studentList()
     {
-        $this->db->mysql->query("INSERT INTO `students` (`name`) VALUES ('Joana')");
-        $this->db->mysql->query("INSERT INTO `students` (`name`) VALUES ('Fritz')");
+        $this->db->mysql->query("INSERT INTO `students` (`name`) VALUES ('Alex');");
+        $this->db->mysql->query("INSERT INTO `students` (`name`) VALUES ('Cintia');");
+
         $studentList = Student::all();
 
-        $this->assertEquals("Joana", $studentList[0]->getName());
-        $this->assertEquals("Fritz", $studentList[1]->getName());
-        $this->assertEquals(1, $studentList[0]->getId());
+        $this->assertEquals("Alex", $studentList[0]->getName());
+        $this->assertEquals("Cintia", $studentList[1]->getName());
         $this->assertEquals(2, $studentList[1]->getId());
         $this->assertIsString($studentList[1]->getCreatedAt());
     }
