e71947c2e4ddccf415825e28f5977b4ae382d436
Sergi Virgili <svirgilif@gmail.com>
2020-11-16 20:54:51 +0100

    add htaccess

--- src/index.php
+++ src/index.php
@@ -6,3 +6,5 @@ use App\Controllers\StudentController;
 
 
 $controller = new StudentController();
+// $base_url = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER["REQUEST_URI"] . '?') . '/';
+// var_dump($base_url);
